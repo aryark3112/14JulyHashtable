f3b6d885d83f2b18d922603e2bf35109134fd59f
aryark <aryark3112@gmail.com>
2023-07-14 11:14:54 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -25,6 +25,33 @@ namespace _14julyCollection
             {
                 Console.WriteLine(item);
             }
+            arrayList.Add("Amit");        //to add two more items to existing arrayList
+            arrayList.Add("Sunitha");
+            Console.WriteLine("After adding two more elements in array list");
+            Console.WriteLine("Number of Element in Array List are:\t" + arrayList.Count);
+            Console.WriteLine("Capacity of Array List is:\t" + arrayList.Capacity);
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            arrayList.Add("sona");        //to add three more items to existing arrayList
+            arrayList.Add("manju");
+            arrayList.Add("roy");
+            Console.WriteLine("After adding 8 elements in array list");
+            Console.WriteLine("Number of Element in Array List are:\t" + arrayList.Count);
+            Console.WriteLine("Capacity of Array List is:\t" + arrayList.Capacity);
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            arrayList.Add("Raj");         //After adding 1 more item in arrayList
+            Console.WriteLine("After adding one more item in array list");
+            Console.WriteLine("Number of Element in Array List are:\t" + arrayList.Count);
+            Console.WriteLine("Capacity of Array List is:\t" + arrayList.Capacity);
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadKey();
         }
     }
